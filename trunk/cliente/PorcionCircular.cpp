f0c4439c490d85871f8d0d46b58bc80759912f11
sebastian.j.bogado@gmail.com <sebastian.j.bogado@gmail.com@e64dc92d-853a-ab2f-13e5-ae16fc65aa46>
2012-06-06 07:59:45 +0000

    Conectados los gráficos a la navegación, falta implementar

--- trunk/cliente/PorcionCircular.cpp
+++ trunk/cliente/PorcionCircular.cpp
@@ -8,27 +8,40 @@ PorcionCircular::PorcionCircular(const Hecho& dato, double maximo, unsigned i,
     x0 = _x0;
     y0 = _y0;
     r = radio;
+    valor =  2*M_PI*(dato.getValor()/max);
 }
 
 PorcionCircular::~PorcionCircular() {}
 
 double PorcionCircular::dibujar(Cairo::RefPtr< Cairo::Context >& ctx,
                                 double offset) {
-    double hasta = offset + 2*M_PI*(dato.getValor()/max);
     ctx->save();
         ctx->set_source_rgba(color[0], color[1], color[2], color[3]);
         ctx->move_to(x0, y0);
-        ctx->arc(x0, y0, r, offset, hasta);
+        ctx->arc(x0, y0, r, offset, offset + valor);
         ctx->move_to(x0, y0);
         ctx->close_path();
         ctx->fill_preserve();
         ctx->set_source_rgba(0.0, 0.0, 0.0, 1.0);
         ctx->stroke();
     ctx->restore();
-    return hasta;
+    return offset + valor;
 }
 
 bool PorcionCircular::fueClickeada(double x, double y, double& offset) {
-    return false;
+    x -= x0;
+    y -= y0;
+    double angulo = atan2(y, x);
+    if (y < 0)
+        angulo += 2*M_PI;
+    bool clickeada;
+    if (sqrt(x*x + y*y) < r &&
+        offset < angulo && angulo < offset + valor)
+        clickeada = true;
+    else
+        clickeada = false;
+
+    offset += valor;
+    return clickeada;
 }
 

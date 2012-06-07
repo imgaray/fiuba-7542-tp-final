7f494ec6f070e628b44b80a7e6b5835431c81203
imgarayojeda@gmail.com <imgarayojeda@gmail.com@e64dc92d-853a-ab2f-13e5-ae16fc65aa46>
2012-06-07 19:50:52 +0000

    refactorizacion de servidor

--- trunk/servidor/VerificadorConsulta.cpp
+++
@@ -1,9 +0,0 @@
-#include "VerificadorConsulta.h"
-
-VerificadorConsulta::VerificadorConsulta() {
-	//TODO(nacho): constructor de verificador de consultas
-}
-
-VerificadorConsulta::~VerificadorConsulta() {
-	//TODO(nacho): destructor de verificador de consultas
-}

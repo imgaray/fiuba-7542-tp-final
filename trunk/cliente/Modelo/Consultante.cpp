4b2d810500faa5c348cede2b75cebc983a4a13af
sebastian.j.bogado@gmail.com <sebastian.j.bogado@gmail.com@e64dc92d-853a-ab2f-13e5-ae16fc65aa46>
2012-06-08 17:51:40 +0000

    Completada la interfaz Consultante

--- trunk/cliente/Modelo/Consultante.cpp
+++ trunk/cliente/Modelo/Consultante.cpp
@@ -1,5 +1,6 @@
 #include "Consultante.h"
 #include "../../comun/Respuesta.h"
+#include <iostream>
 
 unsigned Consultante::generadorID = 0;
 
@@ -8,3 +9,20 @@ Consultante::Consultante() {
     esperandoRespuesta = false;
 }
 
+Consultante::~Consultante() {}
+
+void Consultante::hacerConsulta() const {
+    if (esperandoRespuesta)
+        // cancelar
+        std::cout << "Consulta anterior cancelada" << std::endl;
+    esperandoRespuesta = true;
+}
+
+void Consultante::recibirRespuesta(const Respuesta& rta) {
+    procesarRespuesta(rta);
+    esperandoRespuesta = false;
+}
+
+unsigned Consultante::getID() const {
+    return ID;
+}

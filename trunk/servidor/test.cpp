a6b14b3b6a13dc6e8529cb541a854028034ddf34
imgarayojeda@gmail.com <imgarayojeda@gmail.com@e64dc92d-853a-ab2f-13e5-ae16fc65aa46>
2012-06-08 14:35:42 +0000

    Agregado set de pruebas del servidor (combinado con la base de datos)

--- trunk/servidor/test.cpp
+++ trunk/servidor/test.cpp
@@ -5,10 +5,110 @@
 #include "Consulta.h"
 #include "Respuesta.h"
 #include "Organizacion.h"
+#include "Hilo.h"
+#include "Servidor.h"
+
+#define MAX_CLIENTES_SIMULTANEOS 2
+#define MAX_AGENTES_SIMULTANEOS 2
+
+std::string vendedores[] = {"Seba", "Migue", "Piba", "Chabon", "Nacho"};
+std::string sucursales[] = {"Mordor", "Varela", "Moron", "Venus", "Lomas"};
+std::string fechas[] = {"20111212", "20080909", "20081230", "20120101", "19900502" };
+std::string productos[] = {"chupetin", "notebook", "ferrari", "felicidad", "titulos de ingeniero"};
 
 void assert_prueba(bool resultado) {
-	std::cout << ((resultado)? "OK" : "ERROR") << std::endl;
+	std::cout << ((resultado)? "OK PRUEBA" : "ERROR PRUEBA") << std::endl;
 }
+
+void imprimirRespuesta(Respuesta& r) {
+	for (unsigned i = 0; i < r.cantidadFilas(); ++i) {
+		std::cout << "::";
+		for (unsigned j = 0; j < r.cantidadColumnas(); ++j) {
+			std::cout.width(11);
+			std::cout.fill('.');
+			std::cout << r.dato(i,j) << "|";
+		}
+		std::cout << std::endl;
+	}
+}
+
+class clienteDummy: public Hilo {
+private:
+	static unsigned instancias;
+	unsigned id;
+public:
+	clienteDummy() {
+		id = ++instancias;
+	}
+	~clienteDummy() {}
+	void correr() {
+		std::cout << "=========PRUEBA CLIENTE EN THREAD " << id << " ==========" << endl;
+		Consulta c[5];
+		for (unsigned i = 0; i < 5; ++i) {
+			c[i].definirComoConsultaCliente();
+			c[i].agregarFiltro("Vendedor", vendedores[i].c_str());
+			c[i].agregarFiltro("Sucursal", sucursales[i].c_str());
+			c[i].agregarResultado("Vendedor");
+		}
+		Socket* sock = new Socket((Puerto)4321);
+		std::string host("localhost");
+		sock->conectar(host);
+		Respuesta r;
+		for (unsigned i = 0; i < 5; ++i) {
+			std::cout << "conectando con el servidor:" << std::endl;
+			assert_prueba(sock->conectado());
+			std::cout << "mandando dato " << i << std::endl;
+			assert_prueba(sock->enviar(c[i]));
+			std::cout << "esperando respuesta " << i << std::endl;
+			assert_prueba(sock->recibir(r));
+			imprimirRespuesta(r);
+		}
+		sock->desconectar();
+		delete sock;
+	}
+};
+
+unsigned clienteDummy::instancias = 0;
+
+class agenteDummy: public Hilo {
+private:
+	static unsigned instancias;
+	unsigned id;
+public:
+	agenteDummy() {
+		id = ++instancias;
+	}
+	~agenteDummy() {}
+	void correr() {
+		std::cout << "=========PRUEBA AGENTE EN THREAD " << id << " ==========" << endl;
+		Consulta c[5];		
+		for (unsigned i = 0; i < 5; ++i) {
+			c[i].definirComoConsultaAgente();
+			c[i].agregarCampo(vendedores[i].c_str());
+			c[i].agregarCampo(sucursales[i].c_str());
+			c[i].agregarCampo(fechas[i].c_str());
+			c[i].agregarCampo(productos[i].c_str());
+		}
+		Socket* sock = new Socket((Puerto)12345);
+		std::string host("localhost");
+		sock->conectar(host);
+		Respuesta r;
+		for (unsigned i = 0; i < 5; ++i) {
+			std::cout << "conectando con el servidor:" << std::endl;
+			assert_prueba(sock->conectado());
+			std::cout << "mandando dato " << i << std::endl;
+			assert_prueba(sock->enviar(c[i]));
+			std::cout << "esperando respuesta "<< i << std::endl;
+			assert_prueba(sock->recibir(r));
+			// imprimirRespuesta(r);
+			std::cout << r.mensajeInterno();
+		}
+		sock->desconectar();
+		delete sock;
+	}
+};
+
+unsigned agenteDummy::instancias = 0;
 
 void inicializar() {
 	const char* rutaOrganizacion = "./organizacion.txt";
@@ -22,8 +122,8 @@ void inicializar() {
 	Organizacion::cargarDefiniciones(rutaOrganizacion);
 }
 
-int main(int argc, char **argv) {
-	inicializar();
+
+void pruebaAgentes() {
 	Consulta c[5];
 	std::string vendedores[] = {"Seba", "Migue", "Piba", "Chabon", "Nacho"};
 	std::string sucursales[] = {"Mordor", "Varela", "Moron", "Venus", "Lomas"};
@@ -43,28 +143,86 @@ int main(int argc, char **argv) {
 	for (unsigned i = 0; i < 5; ++i) {
 		std::cout << "conectando con el servidor:" << std::endl;
 		assert_prueba(sock->conectado());
-		std::cout << "mandando dato 1:" << std::endl;
+		std::cout << "mandando dato " << i << std::endl;
 		assert_prueba(sock->enviar(c[i]));
-		std::cout << "esperando respuesta 1:" << std::endl;
+		std::cout << "esperando respuesta "<< i << std::endl;
 		assert_prueba(sock->recibir(r));
-		for (unsigned i = 0; i < r.cantidadFilas(); ++i) {
-			std::cout << "::";
-			for (unsigned j = 0; j < r.cantidadColumnas(); ++j) {
-				std::cout.width(11);
-				std::cout.fill('.');
-				std::cout << r.dato(i,j) << "|";
-			}
-			std::cout << std::endl;
-		}
-	}
-	std::string a("");
-	std::string q("q");
-	while (a != q) {
-		std::cout << "ingrese 'q' para salir: ";
-		std::cin >> a;
-		std::cout << std::endl;
+		imprimirRespuesta(r);
 	}
 	sock->desconectar();
 	delete sock;
+}
+
+void pruebaClientes() {
+	std::cout << "===========PRUEBA CLIENTE SIMPLE===========" << std::endl;
+	Consulta c[5];
+	for (unsigned i = 0; i < 5; ++i) {
+		c[i].definirComoConsultaCliente();
+		c[i].agregarFiltro("Vendedor", vendedores[i].c_str());
+		c[i].agregarFiltro("Sucursal", sucursales[i].c_str());
+	}
+	Socket* sock = new Socket((Puerto)4321);
+	std::string host("localhost");
+	sock->conectar(host);
+	Respuesta r;
+	for (unsigned i = 0; i < 5; ++i) {
+		std::cout << "conectando con el servidor:" << std::endl;
+		assert_prueba(sock->conectado());
+		std::cout << "mandando dato " << i << std::endl;
+		assert_prueba(sock->enviar(c[i]));
+		std::cout << "esperando respuesta " << i << std::endl;
+		assert_prueba(sock->recibir(r));
+		imprimirRespuesta(r);
+	}
+	sock->desconectar();
+	delete sock;
+}
+
+void pruebaAgentesMultiples() {
+	agenteDummy agentes[MAX_AGENTES_SIMULTANEOS];
+	for (unsigned i = 0; i < MAX_AGENTES_SIMULTANEOS; ++i) {
+		agentes[i].iniciar();
+	}
+	for (unsigned i = 0; i < MAX_AGENTES_SIMULTANEOS; ++i) {
+		agentes[i].sincronizar();
+	}
+}
+
+void pruebaClientesMultiples() {
+	clienteDummy clientes[MAX_CLIENTES_SIMULTANEOS];
+	agenteDummy agentes[MAX_AGENTES_SIMULTANEOS];
+	for (unsigned i = 0; i < MAX_CLIENTES_SIMULTANEOS; ++i)
+		clientes[i].iniciar();
+	for (unsigned i = 0; i < MAX_AGENTES_SIMULTANEOS; ++i)
+		agentes[i].iniciar();
+	for (unsigned i = 0; i < MAX_CLIENTES_SIMULTANEOS; ++i)
+		clientes[i].sincronizar();
+	for (unsigned i = 0; i < MAX_AGENTES_SIMULTANEOS; ++i)
+		agentes[i].sincronizar();	
+}
+
+void pruebaAmbosMultiples() {
+	clienteDummy clientes[MAX_CLIENTES_SIMULTANEOS];
+	for (unsigned i = 0; i < MAX_CLIENTES_SIMULTANEOS; ++i) {
+		clientes[i].iniciar();
+	}
+	for (unsigned i = 0; i < MAX_CLIENTES_SIMULTANEOS; ++i) {
+		clientes[i].sincronizar();
+	}
+}
+
+int main(int argc, char **argv) {
+	inicializar();
+	Servidor server;
+	cout << "===================================================" << endl;
+	pruebaAgentes();
+	cout << "===================================================" << endl;
+	pruebaClientes();
+	cout << "===================================================" << endl;
+	pruebaAgentesMultiples();
+	cout << "===================================================" << endl;
+	pruebaClientesMultiples();
+	cout << "===================================================" << endl;
+	pruebaAmbosMultiples();
 	return 0;
 }

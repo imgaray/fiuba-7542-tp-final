604b8679865c6c0064028ee34b68f9a722756bb1
imgarayojeda@gmail.com <imgarayojeda@gmail.com@e64dc92d-853a-ab2f-13e5-ae16fc65aa46>
2012-06-10 22:27:31 +0000

    me comi los .cpp y el makefile, ahi los subi

---
+++ trunk/cliente/Modelo/ConsumerRespuestas.cpp
@@ -0,0 +1,20 @@
+#include "ConsumerRespuestas.h"
+
+void ConsumerRespuesta::correr() {
+	while (corriendo()) {
+		try {
+			ParRespuesta prespuesta = respuestas.pop2();
+			consultantes[prespuesta.first]->recibirRespuesta(Respuesta(*prespuesta.second));
+			delete prespuesta.second;
+		} catch (...){
+			parar();
+		}				
+	}
+}
+
+ConsumerRespuesta::ConsumerRespuesta(ColaRespuestas& cresp, MapaConsultantes& mcons):
+	respuestas(cresp), consultantes(mcons) {
+}
+	
+ConsumerRespuesta::~ConsumerRespuesta() {
+}

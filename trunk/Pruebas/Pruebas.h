b898eb5b89c869e11e2129e815f9b61b8380556f
mat1204.torres@gmail.com <mat1204.torres@gmail.com@e64dc92d-853a-ab2f-13e5-ae16fc65aa46>
2012-06-10 06:26:09 +0000

    Algunas pruebas Subidas

---
+++ trunk/Pruebas/Pruebas.h
@@ -0,0 +1,24 @@
+/*
+ * Pruebas.h
+ *
+ *  Created on: 03/06/2012
+ *      Author: migue
+ */
+
+#ifndef PRUEBAS_H_
+#define PRUEBAS_H_
+
+void testOrganizacion();
+
+void testArchivoDeDatos();
+
+void testInterseccion();
+void testFiltrar();
+void testAgregacion();
+
+
+void testValidadorConsulta();
+
+void testBaseDeDatos();
+
+#endif /* PRUEBAS_H_ */

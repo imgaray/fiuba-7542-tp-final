cf9183aa5e513883b9adbaf42bee30241b975829
imgarayojeda@gmail.com <imgarayojeda@gmail.com@e64dc92d-853a-ab2f-13e5-ae16fc65aa46>
2012-06-11 03:08:01 +0000

    agregado el archivo de configuracion

---
+++ trunk/comun/ArchivoConfiguracion.cpp
@@ -0,0 +1,30 @@
+#include "ArchivoConfiguracion.h"
+
+#define SEPARADOR '='
+
+// por lo pronto, el formato correcto sera que haya un solo separador
+
+bool formatoCorrecto(std::string& linea) {
+	return (linea.find_first_of(SEPARADOR) == linea.find_last_of(SEPARADOR));
+}
+
+ArchivoConfiguracion::ArchivoConfiguracion(char* ruta) {
+	std::fstream archivo(ruta);
+	if (!archivo) {
+		// manejar el caso de primer uso
+	} else {
+		std::string aux;
+		while(!archivo.eof()) {
+			std::getline(archivo, aux);
+			if (formatoCorrecto(aux)) {
+				std::string nombre = aux.substr(aux.find_first_of(SEPARADOR) - 1);
+				std::string atributo = aux.substr(aux.find_first_of(SEPARADOR) + 1);
+				if (nombre.size() && atributo.size())
+					atributos[nombre] = atributo;
+			}
+		}
+	}
+	archivo.close();
+}
+
+ArchivoConfiguracion::~ArchivoConfiguracion() {}

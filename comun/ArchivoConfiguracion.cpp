3caef02e45a59d3d44de22b5837cc87dfbde044d
imgarayojeda <imgarayojeda@gmail.com>
2012-06-12 02:41:23 +0000

    borre un metodo que se me agrego en el merge

--- comun/ArchivoConfiguracion.cpp
+++ comun/ArchivoConfiguracion.cpp
@@ -2,13 +2,20 @@
 
 #define SEPARADOR '='
 
-// por lo pronto, el formato correcto sera que haya un solo separador
-
-bool ArchivoConfiguracion::formatoCorrecto(std::string& linea) {
-	return (linea.find_first_of(SEPARADOR) == linea.find_last_of(SEPARADOR));
+ArchivoConfiguracion::iterator ArchivoConfiguracion::begin() {
+	return atributos.begin();
 }
 
-ArchivoConfiguracion::ArchivoConfiguracion(char* ruta) {
+ArchivoConfiguracion::iterator ArchivoConfiguracion::end() {
+	return atributos.end();
+}
+
+bool ArchivoConfiguracion::formatoCorrecto(std::string& linea) {
+	return (linea.find(SEPARADOR) != std::string::npos && 
+	(linea.find_first_of(SEPARADOR) == linea.find_last_of(SEPARADOR)));
+}
+
+ArchivoConfiguracion::ArchivoConfiguracion(char* ruta): rutaArchivo(ruta) {
 	std::fstream archivo(ruta);
 	if (!archivo) {
 		// manejar el caso de primer uso
@@ -17,8 +24,9 @@ ArchivoConfiguracion::ArchivoConfiguracion(char* ruta) {
 		while(!archivo.eof()) {
 			std::getline(archivo, aux);
 			if (formatoCorrecto(aux)) {
-				std::string nombre = aux.substr(aux.find_first_of(SEPARADOR) - 1);
-				std::string atributo = aux.substr(aux.find_first_of(SEPARADOR) + 1);
+				unsigned int posSeparador = aux.find_first_of(SEPARADOR);
+				std::string nombre = aux.substr(0, posSeparador - 1);
+				std::string atributo = aux.substr(posSeparador + 1);
 				if (nombre.size() && atributo.size())
 					atributos[nombre] = atributo;
 			}

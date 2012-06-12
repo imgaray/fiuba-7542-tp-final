c2cface511296af14b1ca58316ee665bb101e8d4
imgarayojeda <imgarayojeda@gmail.com>
2012-06-12 17:52:24 +0000

    impresiones de debug

--- cliente/Modelo/ConsumerConsultas.cpp
+++ cliente/Modelo/ConsumerConsultas.cpp
@@ -1,5 +1,6 @@
 #include "ConsumerConsultas.h"
 #include <sstream>
+#include <iostream>
 
 template <class T, class Y>
 void convertir(T& objetivo, Y& destino) {
@@ -17,13 +18,16 @@ void ConsumerConsulta::correr() {
 	while (corriendo()) {
 		if (servidor && servidor->conectado()) {
 			try {
+				std::cout << "estoy en consumer consulta, procesando" << std::endl;
 				bool error = false;
 				ParConsulta pconsulta = consultas.pop2();
+				std::cout << "pase el pop2, tengo una consulta para " << pconsulta.first << std::endl;
 				ParRespuesta parrta;
 				parrta.first = pconsulta.first;
 				parrta.second = new Respuesta();
 				if (servidor->enviar(*pconsulta.second)) {
-					error = servidor->recibir(*parrta.second);
+					error = servidor->recibir(*(parrta.second));
+					std::cout << "recibi la respuesta para "<< parrta.first << std::endl;
 				}
 				delete pconsulta.second;
 				if (error) {
@@ -32,6 +36,7 @@ void ConsumerConsulta::correr() {
 					respuestas.push(parrta);
 				}
 			} catch(...) {
+				std::cout << "estan deteniendo el consumer consulta" << std::endl;
 				detener_hilo();
 			}
 		}

a25d7a4d33941bc0040241a3c78d3c6a20b48283
imgarayojeda@gmail.com <imgarayojeda@gmail.com@e64dc92d-853a-ab2f-13e5-ae16fc65aa46>
2012-06-12 22:09:24 +0000

    servidor remoto sin chiches

--- trunk/cliente/Modelo/ConsumerConsultas.h
+++ trunk/cliente/Modelo/ConsumerConsultas.h
@@ -8,9 +8,7 @@
 #include "Socket.h"
 #include "ArchivoConfiguracion.h"
 #define MAX_OPEN_PORTS 10
-#define RUTACONFIGURACIONSR "servRemoto.conf"
-#define ATRIBPUERTO "puerto_servidor"
-#define ATRIBDIRECCION "direccion_servidor"
+
 
 class Socket;
 

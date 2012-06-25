76ae50942cc93a233a08c463e882a148c63cba3e
mat1204.torres <mat1204.torres@gmail.com>
2012-06-25 00:42:28 +0000

    Agregado de navegavilidad a la Tabla Comun

--- cliente/Vista/TablaComun.cpp
+++ cliente/Vista/TablaComun.cpp
@@ -6,8 +6,13 @@
  */
 
 #include "TablaComun.h"
+#include "Tab.h"
 #include <iostream>
 #include <string>
+//#include <gtkmm-2.4/gtkmm/treepath.h>
+//#include <gtkmm-2.4/gtkmm/treeview.h>
+//#include <gtkmm-2.4/gtkmm/treeviewcolumn.h>
+//#include <gtkmm-2.4/gtkmm/treemodelcolumn.h>
 
 void mensaje(const std::string& msj) {
 	std::cout << "++++++++++++++++++++++++CLIENTE: " << msj << std::endl;
@@ -40,6 +45,7 @@ TablaComun::TablaComun(FiltradoresPanel& filtradores) : Tabla(filtradores) {
 
 	this->signal_row_activated().connect(sigc::mem_fun(*this,
 	        &TablaComun::fila_activada));
+
 }
 
 TablaComun::~TablaComun() {
@@ -50,6 +56,9 @@ void TablaComun::procesarRespuesta(const Respuesta& rta) {
 	mensaje("Se entro a procesar la Respuesta");
 
 	__imprimirRespuesta(rta, "Respuesta De TablaComun");
+
+	_respActual.limpiar();
+	_respActual = rta;
 
 	if (_colModelo != NULL) {
 		this->remove_all_columns();
@@ -67,6 +76,8 @@ void TablaComun::procesarRespuesta(const Respuesta& rta) {
 	 }
 
 	agregarColumnas();
+
+	this->columns_autosize();
 
 	mensaje("Se termino el procesarRespuesta");
 }
@@ -87,4 +98,37 @@ void TablaComun::agregarColumnas() {
 }
 void TablaComun::fila_activada(const Gtk::TreeModel::Path& path, Gtk::TreeViewColumn* column) {
 	std::cout << "/////////////////////////////SE ACTIVO UNA FILA//////////////////" << std::endl;
+
+//	std::cout << "Direccion de COlumna: "<< column << std::endl;
+//	std::cout << "Nombre COl: " <<column->get_title() << std::endl;
+//	std::cout << "A string de Path: " << path.to_string() << std::endl;
+
+	std::string dimension, valor;
+
+//	for (unsigned i = 0; i < path.size() ; i++) {
+//
+//		Gtk::TreePath::const_reference referencia = path[i];
+//		std::cout << "Referencia al Path " << i <<": " << referencia << std::endl;
+//	}
+
+	int col = 0;
+	bool colEncontrada = false;
+	for (int  i = 0; i < this->_colModelo->_cantCol && !colEncontrada; i++) {
+		if (column == this->get_column(i)) {
+			colEncontrada = true;
+			col = i;
+		}
+	}
+
+	if (!colEncontrada)
+		throw "COLUMAN NO ENCONTRADA PARA NAVEGACION DE TABLA COMUN";
+
+//	std::cout << "TIPO: " << column->get_title() << std::endl;
+//	std::cout << "COLUMNA: " << col << std::endl;
+//	std::cout << "DATO: " << _respActual.dato(path[0], col) << std::endl;
+
+	dimension = column->get_title();
+	valor = _respActual.dato(path[0], col);
+
+	padre->difundirNavegacionSeleccionada(dimension, valor);
 }

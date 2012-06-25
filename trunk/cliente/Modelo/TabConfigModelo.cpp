5779cd13502884fe847f06f31d940ae616402332
sebastian.j.bogado@gmail.com <sebastian.j.bogado@gmail.com@e64dc92d-853a-ab2f-13e5-ae16fc65aa46>
2012-06-25 08:56:21 +0000

    Bugfix en AdminConfigObjManager; agregada interfaz ConfigModelo

--- trunk/cliente/Modelo/TabConfigModelo.cpp
+++ trunk/cliente/Modelo/TabConfigModelo.cpp
@@ -1,37 +1,29 @@
 #include "TabConfigModelo.h"
 
 TabConfigModelo::TabConfigModelo()
-: tabLabel(NOMBRE_TAB_POR_DEFECTO),
-  pEntryTabLabel(NULL),
+: ConfigModelo(NOMBRE_TAB_POR_DEFECTO),
   pSpinButtonFilas(NULL), pSpinButtonCols(NULL) {
     filas = 1; cols = 1;
     min_fila = 1; min_col = 1;
     for (unsigned i = 0; i < MAX_GRILLA; ++i)
         for (unsigned j = 0; j < MAX_GRILLA; ++j)
             ocupacionesGrilla[i][j] = false;
-
-
 }
 
 TabConfigModelo::~TabConfigModelo() {
-
+    desconectar();
 }
+
 #include <iostream>
-void TabConfigModelo::desconectar() {
-    std::cout << "void TabConfigModelo::desconectar() (@todo)" << std::endl;
-    if (pEntryTabLabel)
-        connectionEntryTabLabel.disconnect();
-    if (pSpinButtonFilas)
+void TabConfigModelo::desconectarDeHijo() {
+    if (pSpinButtonFilas) {
         connectionSpinButtonFilas.disconnect();
-    if (pSpinButtonCols)
+        pSpinButtonFilas = NULL;
+    }
+    if (pSpinButtonCols) {
         connectionSpinButtonCols.disconnect();
-}
-
-void TabConfigModelo::setEntryTabLabel(Gtk::Entry* pEntry) {
-    pEntryTabLabel = pEntry;
-    pEntryTabLabel->set_text(tabLabel);
-    connectionEntryTabLabel = pEntryTabLabel->signal_changed().connect(
-        sigc::mem_fun(*this, &TabConfigModelo::on_entry_tab_label_changed));
+        pSpinButtonCols = NULL;
+    }
 }
 
 void TabConfigModelo::setSpinButtonsGrilla(Gtk::SpinButton* pFilas,
@@ -47,25 +39,6 @@ void TabConfigModelo::setSpinButtonsGrilla(Gtk::SpinButton* pFilas,
         sigc::mem_fun(*this, &TabConfigModelo::on_spinbutton_filas_value_changed));
     connectionSpinButtonCols = pSpinButtonCols->signal_value_changed().connect(
         sigc::mem_fun(*this, &TabConfigModelo::on_spinbutton_cols_value_changed));
-}
-
-Glib::ustring TabConfigModelo::getTabLabel() const {
-    return tabLabel;
-}
-
-Glib::ustring TabConfigModelo::getTabLabelNueva() const {
-    if (pEntryTabLabel)
-        return pEntryTabLabel->get_text();
-    else
-        return tabLabel;
-}
-
-void TabConfigModelo::setTabLabelNuevaComoValida() {
-    tabLabel = pEntryTabLabel->get_text();
-}
-
-void TabConfigModelo::on_entry_tab_label_changed() {
-
 }
 
 void TabConfigModelo::on_spinbutton_filas_value_changed() {
--- trunk/cliente/Modelo/TabConfigModelo.h
+++ trunk/cliente/Modelo/TabConfigModelo.h
@@ -1,43 +1,35 @@
 #ifndef TAB_CONFIG_MODELO_H
 #define TAB_CONFIG_MODELO_H
 
-#include <gtkmm/entry.h>
 #include <gtkmm/spinbutton.h>
+#include "ConfigModelo.h"
 
 #define NOMBRE_TAB_POR_DEFECTO "Nueva pestaña"
 #define MAX_GRILLA 99
 
-class TabConfigModelo {
+class TabConfigModelo : public ConfigModelo {
     public:
         TabConfigModelo();
         ~TabConfigModelo();
 
-        void desconectar();
-        void setEntryTabLabel(Gtk::Entry* pEntry);
         void setSpinButtonsGrilla(Gtk::SpinButton* pFilas,
                                   Gtk::SpinButton* pCols);
 
-        Glib::ustring getTabLabel() const;
-        Glib::ustring getTabLabelNueva() const;
-        void setTabLabelNuevaComoValida();
     private:
         /** modelo mismo */
-        Glib::ustring tabLabel;
         unsigned filas, cols;
         unsigned min_fila, min_col;
         bool ocupacionesGrilla[MAX_GRILLA][MAX_GRILLA];
 
         /** conexiones a las señales de la vista */
-        sigc::connection connectionEntryTabLabel;
         sigc::connection connectionSpinButtonFilas, connectionSpinButtonCols;
+        void desconectarDeHijo();
 
         /** signal handlers */
-        void on_entry_tab_label_changed();
         void on_spinbutton_filas_value_changed();
         void on_spinbutton_cols_value_changed();
 
         /** referencias a la vista */
-        Gtk::Entry* pEntryTabLabel;
         Gtk::SpinButton* pSpinButtonFilas;
         Gtk::SpinButton* pSpinButtonCols;
 };

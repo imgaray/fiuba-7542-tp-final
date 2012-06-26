8f697dbef731450012bd3e0bb425d0b2caf9fb48
sebastian.j.bogado <sebastian.j.bogado@gmail.com>
2012-06-26 17:46:57 +0000

    Título por panel

--- cliente/Vista/Panel.h
+++ cliente/Vista/Panel.h
@@ -3,21 +3,23 @@
 
 #include <gtkmm/box.h>
 #include <gtkmm/scrolledwindow.h>
+#include <gtkmm/frame.h>
 #include <gtkmm/viewport.h>
 
 class Grafico;
 class Tabla;
 class FiltradoresPanel;
 
-class Panel : public Gtk::VBox {
+class Panel : public Gtk::Frame {
     public:
-        Panel();
+        Panel(const Glib::ustring& label);
         ~Panel();
 
         // Agrega también a los filtradores, pidiéndoselos al gráfico
         void setContenido(Grafico& g);
         void setContenido(Tabla& t);
     private:
+        Gtk::VBox box;
         Gtk::ScrolledWindow scrWin;
         Gtk::Viewport* pViewport;
 };

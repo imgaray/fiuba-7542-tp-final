73436a24cc29e5b23e89df0d3ae73a420547d2d8
sebastian.j.bogado <sebastian.j.bogado@gmail.com>
2012-06-26 08:41:09 +0000

    Agregado un diálogo de error al inicar la aplicación si hubiera uno

--- cliente/main.cpp
+++ cliente/main.cpp
@@ -1,10 +1,34 @@
-#include "Cliente.h"
 #include <gtkmm/main.h>
+#include <gtkmm/messagedialog.h>
+#include "Cliente.h"
+#include "ExcepcionArchivoGladeCorrupto.h"
+
+#define SIN_ERRORES "____no_hubo_errores____"
 
 int main(int argc, char* argv[]) {
     Gtk::Main kit(argc, argv);
-    Cliente c(argc, argv);
-    c.run();
+    Glib::ustring msj = SIN_ERRORES;
+    try {
+        Cliente c(argc, argv);
+        c.run();
+    }
+    catch (const ExcepcionArchivoGladeCorrupto& e) {
+         msj = e.what();
+    }
+    catch (char const* _msj) {
+        msj = _msj;
+    }
+
+    if (msj != SIN_ERRORES) {
+        msj += "\n\nContáctese con el administrador.";
+        Gtk::MessageDialog dialog(msj,
+                                  false,
+                                  Gtk::MESSAGE_ERROR,
+                                  Gtk::BUTTONS_OK,
+                                  true);
+        dialog.set_title("Error");
+        dialog.run();
+    }
     return 0;
 }
 

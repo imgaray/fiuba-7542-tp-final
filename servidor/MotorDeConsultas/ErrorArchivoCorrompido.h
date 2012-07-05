826c53aed1fc0972442afbdcd6eabded220e6257
mat1204.torres <mat1204.torres@gmail.com>
2012-07-05 16:52:49 +0000

    Error de archivo corrompido

---
+++ servidor/MotorDeConsultas/ErrorArchivoCorrompido.h
@@ -0,0 +1,16 @@
+/*
+ * ErrorArchivoCorrompido.h
+ *
+ *  Created on: 05/07/2012
+ *      Author: migue
+ */
+
+#ifndef ERRORARCHIVOCORROMPIDO_H_
+#define ERRORARCHIVOCORROMPIDO_H_
+
+#include <exception>
+
+class ErrorArchivoCorrompido : public std::exception {
+};
+
+#endif /* ERRORARCHIVOCORROMPIDO_H_ */

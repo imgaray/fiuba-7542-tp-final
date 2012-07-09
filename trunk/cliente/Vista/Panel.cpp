4ff126a3ce66e4c64b98d0c62afaf77d650ef970
sebastian.j.bogado@gmail.com <sebastian.j.bogado@gmail.com@e64dc92d-853a-ab2f-13e5-ae16fc65aa46>
2012-07-09 08:27:06 +0000

    Agregado spinner a cada panel

--- trunk/cliente/Vista/Panel.cpp
+++ trunk/cliente/Vista/Panel.cpp
@@ -4,8 +4,15 @@
 #include "FiltradoresPanel.h"
 #include "Respuesta.h"
 
-Panel::Panel(const Glib::ustring& label) : Gtk::Frame(label) {
+Panel::Panel(const Glib::ustring& label) :etiqueta(label) {
+    etiquetaCompuesta.pack_start(spinner, false, false);
+    etiquetaCompuesta.pack_start(etiqueta, false, false);
+    etiqueta.show();
+    spinner.hide();
+    spinner.set_no_show_all();
+    set_label_widget(etiquetaCompuesta);
     set_label_align(0.5);
+
     add(box);
     scrWin.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
     pViewport = manage(new Gtk::Viewport(*scrWin.get_hadjustment(),
@@ -17,6 +24,7 @@ Panel::~Panel() {}
 
 void Panel::setContenido(Grafico& g) {
     consultante = &g;
+    consultante->setSpinner(&spinner);
     box.pack_start(*(g.getFiltrador()), false, false);
     pViewport->add(g);
     box.add(scrWin);
@@ -24,6 +32,7 @@ void Panel::setContenido(Grafico& g) {
 
 void Panel::setContenido(Tabla& t) {
     consultante = &t;
+    consultante->setSpinner(&spinner);
     box.pack_start(*(t.getFiltrador()), false, false);
     pViewport->add(t);
     box.add(scrWin);
